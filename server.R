ed2740e9b367ff0a1e101664fe3f8dc1cb46a561
Milan <milancrone9898@gmail.com>
2018-12-04 01:16:05 -0800

    Added Histo

--- server.R
+++ server.R
@@ -1,5 +1,29 @@
 library(shiny)
+library(dplyr)
+library(ggvis)
+library(openair)
 
 server <- function(input, output) {
+  
+  crimeDataFull <- read.csv("Crime_Data.csv",  stringsAsFactors = FALSE)
+  crimeDataCut <- select(crimeDataFull, Occurred.Date, Crime.Subcategory, Precinct, Neighborhood)
+  crimeDataReact <- reactive({filter(crimeDataCut,
+                                    as.Date(Occurred.Date, "%m/%d/%Y") %in% seq(as.Date(input$dateRange[1], "yyyy-mm-dd"), as.Date(input$dateRange[2], "yyyy-mm-dd"), by = "1 day"),
+                                    Precinct == input$selectPrecinct,
+                                    Crime.Subcategory != "")
+                            })
+  
+  reactive({crimeDataReact %>% 
+      ggvis(~factor(Crime.Subcategory), fill = ~factor(Crime.Subcategory)) %>%
+      add_axis("x", 
+               title = "Crime", 
+               tick_padding = 90, 
+               properties = axis_props(labels = list(angle = 300, fontSize = 10))) %>%
+      add_axis("y", 
+               title = "Count", 
+               properties = axis_props(labels = list(angle = 345, fontSize = 10))) %>% 
+      set_options(height = 500,
+                  width = 1920)
+        }) %>% bind_shiny("ggvis", "ggvis_ui")
   
 }
--- ui.R
+++ ui.R
@@ -1,9 +1,35 @@
 library(shiny)
+library(dplyr)
+library(ggvis)
 
+
+crimeDataFull <- read.csv("Crime_Data.csv",  stringsAsFactors = FALSE)
+crimeDataCut <- select(crimeDataFull, Occurred.Date, Crime.Subcategory, Precinct, Neighborhood)
+precinctOption <- unique(crimeDataCut$Precinct)
+precinctOption <- precinctOption[1:6]
+precinctOption <- precinctOption[-5]
+  
 ui <- fluidPage(
   navbarPage("Crimes in Seattle",
-             tabPanel("Overview"), 
-             tabPanel("Histogram"),
+             tabPanel("Overview"),
+             tabPanel("Histogram", 
+                sidebarLayout(
+                  sidebarPanel(
+                    radioButtons("selectPrecinct", "Precinct", precinctOption),
+                    dateRangeInput("dateRange", 
+                                   label = "Date Range",
+                                   start = "12/13/1908",
+                                   min = "12/13/1908",
+                                   format = "mm/dd/yyyy"
+                                   )
+                  ),
+                  mainPanel(
+                    textOutput("test"),
+                    uiOutput("ggvis_ui"),
+                    ggvisOutput("ggvis")
+                  )
+                )
+             ),
              tabPanel("Map"), 
              tabPanel("Rankings")
   )
