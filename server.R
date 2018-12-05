dd712ad33b2aad4053a1192f91c7063550673e55
Akshat Pandey <akshat.pnd@gmail.com>
2018-12-04 22:02:32 -0800

    Fixed error

--- server.R
+++ server.R
@@ -2,7 +2,6 @@ library(shiny)
 library(dplyr)
 library(ggvis)
 library(openair)
-<<<<<<< HEAD
 library(leaflet)
 
 
@@ -16,47 +15,33 @@ server <- function(input, output) {
                                      Precinct == input$selectPrecinct,
                                      Crime.Subcategory != "")
   })
-=======
+    
+    
+      reactive({crimeDataReact %>% 
+          ggvis(~factor(Crime.Subcategory), fill = ~factor(Crime.Subcategory)) %>%
+          add_axis("x", 
+                   title = "Crime", 
+                   tick_padding = 90, 
+                   properties = axis_props(labels = list(angle = 300, fontSize = 10))) %>%
+          add_axis("y", 
+                   title = "Count", 
+                   properties = axis_props(labels = list(angle = 345, fontSize = 10))) %>% 
+          set_options(height = 500,
+                      width = 1920)
+      }) %>% bind_shiny("ggvis", "ggvis_ui")
+      
+      get_specific_map_data <- reactive({
+        df <- filter(map_data, map_data$Event.Clearance.Group == input$select)
+      })
+      
+      output$map <- renderLeaflet({
+        m_data <- get_specific_map_data()
+        leaflet(m_data) %>%
+          addTiles() %>%
+          setView(-122.3320708, 47.6062095, zoom = 11) %>%
+          addCircles(m_data$Longitude, m_data$Latitude, weight = 2, popup = paste0(m_data$Hundred.Block.Location, " - ",
+                                                                                   m_data$Event.Clearance.SubGroup), opacity = 0.5)
+      })
 
-server <- function(input, output) {
-  
-  crimeDataFull <- read.csv("Crime_Data.csv",  stringsAsFactors = FALSE)
-  crimeDataCut <- select(crimeDataFull, Occurred.Date, Crime.Subcategory, Precinct, Neighborhood)
-  crimeDataReact <- reactive({filter(crimeDataCut,
-                                    as.Date(Occurred.Date, "%m/%d/%Y") %in% seq(as.Date(input$dateRange[1], "yyyy-mm-dd"), as.Date(input$dateRange[2], "yyyy-mm-dd"), by = "1 day"),
-                                    Precinct == input$selectPrecinct,
-                                    Crime.Subcategory != "")
-                            })
->>>>>>> e72636c6904c54035c7275dc4959386e861be97f
-  
-  reactive({crimeDataReact %>% 
-      ggvis(~factor(Crime.Subcategory), fill = ~factor(Crime.Subcategory)) %>%
-      add_axis("x", 
-               title = "Crime", 
-               tick_padding = 90, 
-               properties = axis_props(labels = list(angle = 300, fontSize = 10))) %>%
-      add_axis("y", 
-               title = "Count", 
-               properties = axis_props(labels = list(angle = 345, fontSize = 10))) %>% 
-      set_options(height = 500,
-                  width = 1920)
-<<<<<<< HEAD
-  }) %>% bind_shiny("ggvis", "ggvis_ui")
-  
-  get_specific_map_data <- reactive({
-    df <- filter(map_data, map_data$Event.Clearance.Group == input$select)
-  })
-  
-  output$map <- renderLeaflet({
-    m_data <- get_specific_map_data()
-    leaflet(m_data) %>%
-      addTiles() %>%
-      setView(-122.3320708, 47.6062095, zoom = 11) %>%
-      addCircles(m_data$Longitude, m_data$Latitude, weight = 2, popup = paste0(m_data$Hundred.Block.Location, " - ",
-                                                                               m_data$Event.Clearance.SubGroup), opacity = 0.5)
-  })
-=======
-        }) %>% bind_shiny("ggvis", "ggvis_ui")
->>>>>>> e72636c6904c54035c7275dc4959386e861be97f
-  
+
 }
--- ui.R
+++ ui.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(dplyr)
 library(ggvis)
-<<<<<<< HEAD
 library(leaflet)
 
 crimeDataFull <- read.csv("Crime_Data.csv",  stringsAsFactors = FALSE)
@@ -9,21 +8,11 @@ crimeDataCut <- select(crimeDataFull, Occurred.Date, Crime.Subcategory, Precinct
 precinctOption <- unique(crimeDataCut$Precinct)
 precinctOption <- precinctOption[1:6]
 precinctOption <- precinctOption[-5]
-=======
->>>>>>> e72636c6904c54035c7275dc4959386e861be97f
 
-
-crimeDataFull <- read.csv("Crime_Data.csv",  stringsAsFactors = FALSE)
-crimeDataCut <- select(crimeDataFull, Occurred.Date, Crime.Subcategory, Precinct, Neighborhood)
-precinctOption <- unique(crimeDataCut$Precinct)
-precinctOption <- precinctOption[1:6]
-precinctOption <- precinctOption[-5]
-  
 ui <- fluidPage(
   navbarPage("Crimes in Seattle",
              tabPanel("Overview"),
              tabPanel("Histogram", 
-<<<<<<< HEAD
                       sidebarLayout(
                         sidebarPanel(
                           radioButtons("selectPrecinct", "Precinct", precinctOption),
@@ -41,50 +30,30 @@ ui <- fluidPage(
                         )
                       )
              ),
-             tabPanel("Map", 
-                      titlePanel(
-                        p("Seattle - Crime Map"),
-                        p("The below contains all the instances of the chosen crime as well as exact location 
-                          when you click on a point.")
-                      ),
-                      sidebarLayout(
-                        sidebarPanel(
-                          helpText(
-                            "Please choose a type of crime to map."
-                          ), 
-                          selectInput("select", label = h3("Type of Crime"),
-                                      choices = list("Assault" = "ASSAULTS", "Auto Thefts" = "AUTO THEFTS", 
-                                                     "Sexual Assault" = "FAILURE TO REGISTER (SEX OFFENDER)", 
-                                                     "Homicide" = "HOMICIDE", "Burglary" = "BURGLARY", 
-                                                     "Property Damage" = "PROPERTY DAMAGE"),
-                                      selected = "Assault"
-                          )
-                        ),
-                        mainPanel(
-                          leafletOutput("map")
-                        )
-                      )
-             ),  
-=======
-                sidebarLayout(
-                  sidebarPanel(
-                    radioButtons("selectPrecinct", "Precinct", precinctOption),
-                    dateRangeInput("dateRange", 
-                                   label = "Date Range",
-                                   start = "12/13/1908",
-                                   min = "12/13/1908",
-                                   format = "mm/dd/yyyy"
-                                   )
-                  ),
-                  mainPanel(
-                    textOutput("test"),
-                    uiOutput("ggvis_ui"),
-                    ggvisOutput("ggvis")
-                  )
-                )
-             ),
-             tabPanel("Map"), 
->>>>>>> e72636c6904c54035c7275dc4959386e861be97f
-             tabPanel("Rankings")
-  )
+tabPanel("Map", 
+         titlePanel(
+           p("Seattle - Crime Map"),
+           p("The below contains all the instances of the chosen crime as well as exact location 
+             when you click on a point.")
+           ),
+         sidebarLayout(
+           sidebarPanel(
+             helpText(
+               "Please choose a type of crime to map."
+             ), 
+             selectInput("select", label = h3("Type of Crime"),
+                         choices = list("Assault" = "ASSAULTS", "Auto Thefts" = "AUTO THEFTS", 
+                                        "Sexual Assault" = "FAILURE TO REGISTER (SEX OFFENDER)", 
+                                        "Homicide" = "HOMICIDE", "Burglary" = "BURGLARY", 
+                                        "Property Damage" = "PROPERTY DAMAGE"),
+                         selected = "Assault"
+             )
+           ),
+           mainPanel(
+             leafletOutput("map")
+           )
+         )
+         ),   
+tabPanel("Rankings")
+)
 )
